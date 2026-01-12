183fe7aa70d7387815d75d16e1912b86921975d8
aphotecavalle-source <aphotecavalle@gmail.com>
2026-01-11 18:31:55 -0600

    Refactor App component and clean up comments
    Removed unnecessary comments and ensured correct file naming for Home component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
+import Home from './Home'; // Asegúrate que el archivo se llame Home.jsx con H mayúscula
 import PropertyVisualizer from './PropertyVisualizer';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Si entran a la raíz (mi-app.vercel.app), ven el buscador elegante */}
         <Route path="/" element={<Home />} />
-        
-        {/* Si entran con código (mi-app.vercel.app/propiedad/casa-1), van directo */}
         <Route path="/propiedad/:id" element={<PropertyVisualizer />} />
       </Routes>
     </Router>
